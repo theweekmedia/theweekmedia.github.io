8a61f65e3c297ac3397cecbc20e36a9f06433f22
theweekmedia <163738239+theweekmedia@users.noreply.github.com>
2024-03-17 18:13:46 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,82 @@
+'use strict';
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+const elemArr = [navCloseBtn, overlay, navOpenBtn];
+
+for (let i = 0; i < elemArr.length; i++) {
+  elemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+/**
+ * toggle navbar & overlay when click any navbar-link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-navbar-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+
+
+
+
+/**
+ * header & go-top-btn active
+ * when window scroll down to 400px
+ */
+
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 400) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const contactForm = document.querySelector('.contact-form');
+
+  contactForm.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    // Get form input values
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const message = document.getElementById('message').value;
+
+    // Basic form validation
+    if (!name || !email || !phone || !message) {
+      alert('Please fill in all fields.');
+      return;
+    }
+
+    // You can perform additional validation or AJAX submission here
+
+    // Clear the form fields after successful submission (replace this with your logic)
+    contactForm.reset();
+    alert('Form submitted successfully!');
+  });
+});
